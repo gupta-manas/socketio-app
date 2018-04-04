dd49b84e062b5ba48b42ee96de2a7dc87c9e7d41
gupta-manas <manas.niet@hotmail.com>
2018-04-04 21:33:25 +0530

    Working chat app

--- public/js/chat.js
+++ public/js/chat.js
@@ -32,12 +32,17 @@ socket.on('disconnect', function() {
     console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(msg) {
-    console.log('New msg: ');
-    console.log('From: ' + msg.from);
-    console.log('Body: ' + msg.text);
-    console.log('Created at: ' + moment(msg.createdAt).format('h:mm:ss a'));
+socket.on('updateUserList', function(users){
+	var ol= jQuery('<ol></ol>');
+	
+	users.forEach(function(user){
+		ol.append(jQuery('<li></li>').text(user));
+	});
+	
+	jQuery('#users').html(ol);
+});
 
+socket.on('newMessage', function(msg) {
     var formattedTime = moment(msg.createdAt).format('h:mm:ss a');
     var template = jQuery('#msg-template').html();
     var html = Mustache.render(template, {
@@ -67,7 +72,6 @@ jQuery('#message-form').on('submit', function(e) {
     var msgTextBox = jQuery('[name=msg]');
 
     socket.emit('createMessage', {
-        from: 'User',
         text: msgTextBox.val()
     }, function() {
         msgTextBox.val('');
--- server/server.js
+++ server/server.js
@@ -4,54 +4,76 @@ const port = process.env.PORT || 3000;
 const socketIO = require('socket.io');
 const http = require('http');
 const publicPath = path.join(__dirname, '../public');
-const { generateMsg, generateLocMsg } = require('./util/msg');
+
+const { generateMessage, generateLocMsg } = require('./util/msg');
 const { isRealString } = require('./util/validation');
+const {Users} = require('./util/users');
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users= new Users();
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected!');
 
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the chat app',
-        createdAt: new Date().getTime()
-    });
-
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user joined',
-        createdAt: new Date().getTime()
-    });
-
     socket.on('join', (params, callback) => {
         if (!isRealString(params.name) || !isRealString(params.room)) {
-            callback("Name and room name are required!")
+            return callback("Name and room name are required!")
         }
-
-        callback();
+		socket.join(params.room);
+		
+		users.removeUser(socket.id);
+		users.addUser(socket.id, params.name, params.room);
+		
+		io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+		
+		socket.emit('newMessage', {
+			from: 'Admin',
+			text: 'Welcome to the chat app',
+			createdAt: new Date().getTime()
+		});
+		
+		socket.broadcast.to(params.room).emit('newMessage', {
+			from: 'Admin',
+			text: `${params.name} has joined`,
+			createdAt: new Date().getTime()
+		});
+        
+		callback();
     });
 
     socket.on('disconnect', () => {
-        console.log('User was disconnected!');
+        var user= users.removeUser(socket.id);
+		if(user){
+			io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+			io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left!`));
+		}
     });
 
     socket.on('createMessage', (newMessage, callback) => {
-        console.log(newMessage);
-        io.emit('newMessage', {
-            from: newMessage.from,
-            text: newMessage.text,
-            createdAt: new Date().getTime()
-        });
+		var user= users.getUser(socket.id);
+		
+		if(user && isRealString(newMessage.text)){
+			io.to(user.room).emit('newMessage', {
+				from: user.name,
+				text: newMessage.text,
+				createdAt: new Date().getTime()
+			});
+		}
+        
         callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocMsg('Admin', coords.latitude, coords.longitude));
+		var user= users.getUser(socket.id);
+		
+		if(user){
+			io.to(user.room).emit('newLocationMessage', generateLocMsg(user.name, coords.latitude, coords.longitude));
+		}
+        
     });
 });
 server.listen(port, () => {
